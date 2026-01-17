50d4cbf553b679c7b04364e2b64f651027e52417
9939937286 <9939937286@users.noreply.github.com>
2026-01-18 01:05:55 +0530

    Add gitignore and remove node_modules

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,7 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+const app = require("./src/app");
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("SkillGo Backend is Running");
-});
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
-  console.log(`SkillGo backend running on port ${PORT}`);
+  console.log(`SkillGo Backend running on port ${PORT}`);
 });
